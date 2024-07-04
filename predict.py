50d5d7e9f564a7a58eddebafef7b527713763d19
BoomSoulhq <52875340+BoomSoulhq@users.noreply.github.com>
2024-07-04 23:54:08 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -14,16 +14,18 @@ from covid_train import DiseaseModel
 def combo_evaluate(model, dti_data, args):
     model.eval()
     all_preds = []
+    column_name = []
     for i in range(5):  # len(dti_data)
-        preds = model.pre_forward([dti_data.data[i].smiles], mode=0)
-        all_preds.append(preds)
-    return np.concatenate(all_preds, axis=0)
+        answer = model.pre_forward([dti_data.data[i].smiles])
+        column_name.append(dti_data.data[0].smiles)
+        all_preds.append(answer)
+    return np.concatenate(all_preds, axis=0), column_name
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument('--batch_size', type=int, default=50)
-    parser.add_argument('--test_path', default=r"D:\users\p30057372\ComboNet\ComboNet-master\data\covid\dti.csv")
+    parser.add_argument('--test_path', default=r"D:\users\p30057372\ComboNet\ComboNet-master\data\covid\dti_test.csv")
     parser.add_argument('--checkpoint_dir', default=r"D:\users\p30057372\ComboNet\ComboNet-master\data\fold_0")
     args = parser.parse_args()
 
@@ -44,7 +46,8 @@ if __name__ == "__main__":
             model = DiseaseModel(ckpt['args'])
             model.load_state_dict(ckpt['state_dict'])
             model_preds = combo_evaluate(model, data, ckpt['args'])
-            df = pd.DataFrame(model_preds)
+            df = pd.DataFrame(model_preds[0])
             pred_output = pd.concat([pred_output, df], ignore_index=True)
+        pred_output.index = model_preds[1]
         pred_output.to_csv(r"D:\users\p30057372\ComboNet\ComboNet-master\data\测试结果{now}.csv".format(
             now=time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))))
