89ce2b4a8b93bfaca3293377a63511f7e8207db3
BoomSoulhq <52875340+BoomSoulhq@users.noreply.github.com>
2024-07-07 19:21:04 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,4 +1,4 @@
-import os, random, sys
+import os,
 import time
 
 import numpy as np
@@ -7,29 +7,35 @@ from argparse import ArgumentParser
 import pandas as pd
 import torch
 
-from chemprop.data.utils import get_data
 from covid_train import DiseaseModel
 
 
-def combo_evaluate(model, dti_data, args):
+def combo_evaluate(model, data, args):
     model.eval()
     all_preds = []
-    column_name = []
-    for i in range(5):  # len(dti_data)
-        answer = model.pre_forward([dti_data.data[i].smiles])
-        column_name.append(dti_data.data[0].smiles)
-        all_preds.append(answer)
-    return np.concatenate(all_preds, axis=0), column_name
+    smile_representations = []
+    for i in range(0, len(data), args.batch_size):
+        mol_batch = data[i: i + args.batch_size]
+        smiles = [smile[0] for smile in mol_batch]
+        cur_smile_representations = model.DTI_forward(smiles).cpu().numpy()
+        preds = torch.sigmoid(model(smiles)).round().cpu().numpy()
+        ans = np.concatenate((cur_smile_representations, preds), axis=1)
+
+        all_preds.append(ans)
+    return np.concatenate(all_preds, axis=0)
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument('--batch_size', type=int, default=50)
-    parser.add_argument('--test_path', default=r"D:\users\p30057372\ComboNet\ComboNet-master\data\covid\dti_test.csv")
-    parser.add_argument('--checkpoint_dir', default=r"D:\users\p30057372\ComboNet\ComboNet-master\data\fold_0")
+    parser.add_argument('--test_path', default='data/covid/synergy_experiment.csv')
+    parser.add_argument('--checkpoint_dir', default="data/trained_model")
     args = parser.parse_args()
+    args.test_path = 'data/covid/synergy_experiment.csv'
 
-    data = get_data(path=args.test_path)
+    with open(args.test_path) as f:
+        header = next(f)
+        data = [line.strip("\r\n ").split(',')[:1] for line in f]
 
     args.checkpoint_paths = []
     for root, _, files in os.walk(args.checkpoint_dir):
@@ -39,15 +45,15 @@ if __name__ == "__main__":
 
     sum_preds = np.zeros((len(data),))
     with torch.no_grad():
-        pred_output = pd.DataFrame()
         for checkpoint_path in args.checkpoint_paths:
             ckpt = torch.load(checkpoint_path)
             ckpt['args'].attention = False
             model = DiseaseModel(ckpt['args'])
+            feature_weights = model.ffn.weight.numpy()
+            feature_weights = np.append(feature_weights[0], ["label"]).reshape(1, 101)
             model.load_state_dict(ckpt['state_dict'])
             model_preds = combo_evaluate(model, data, ckpt['args'])
-            df = pd.DataFrame(model_preds[0])
-            pred_output = pd.concat([pred_output, df], ignore_index=True)
-        pred_output.index = model_preds[1]
-        pred_output.to_csv(r"D:\users\p30057372\ComboNet\ComboNet-master\data\测试结果{now}.csv".format(
-            now=time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))))
+            pred_output = pd.DataFrame(np.concatenate((feature_weights, model_preds), axis=0))
+            pred_output.index = list(["weight"] + [x[0] for x in data])
+            pred_output.to_csv(r"D:\users\p30057372\ComboNet\ComboNet-master\data\测试结果{now}.csv".format(
+                now=time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))))
